8e10ea0bd0b62986edd00b0aada1569114131f95
Timchenko Daniil <dany@timoshka-lab.co.jp>
2022-11-24 12:54:45 +0900

    SSL証明書の自動生成とテストメールサーバ、WPの自動インストールスクリプトを実装

--- docker/app/setup.sh
+++ docker/app/setup.sh
@@ -3,6 +3,7 @@
 set -e
 
 args=()
+SKIP_WP=false
 
 for OPT in "$@"
 do
@@ -12,17 +13,37 @@ do
       args+=("$2")
       shift 2
       ;;
+    --skip-wp)
+      SKIP_WP=true
+      shift 1
+      ;;
   esac
 done
 
 WORDPRESS_DIR=/var/www/html/wordpress/public
 PHPMYADMIN_DIR=/var/www/html/phpmyadmin/public
 
-if [ -f "$WORDPRESS_DIR/index.php" ]; then
-  echo "WordPress is already installed. Skipping..."
+if [ "$SKIP_WP" = false ]; then
+  if [ -f "$WORDPRESS_DIR/index.php" ]; then
+    echo "WordPress is already installed. Skipping..."
+  else
+    WP_USER=""
+    WP_TITLE=""
+
+    while [ -z "$WP_USER" ]; do read -r -p "Username: " WP_USER; done
+    while [ -z "$WP_TITLE" ]; do read -r -p "Website Title: " WP_TITLE; done
+
+    echo "Downloading WordPress..."
+    wp core download --path=$WORDPRESS_DIR --locale=ja --allow-root "${args[@]}"
+
+    echo "Installing WordPress..."
+    wp core install --path=$WORDPRESS_DIR --allow-root --skip-email --url="$WP_SITE_URL" --title="$WP_TITLE" --admin_user="$WP_USER" --admin_email="$WP_EMAIL"
+
+    echo "Installing SMTP plugin..."
+    wp plugin install --path=$WORDPRESS_DIR --allow-root http://github.com/timoshka-lab/wp-dev-smtp/archive/main.zip
+  fi
 else
-  echo "Downloading WordPress..."
-  /usr/local/bin/wp core download --path=$WORDPRESS_DIR --locale=ja --allow-root "${args[@]}"
+  echo "Skipping WordPress installation..."
 fi
 
 if [ -f "$PHPMYADMIN_DIR/index.php" ]; then
@@ -32,3 +53,4 @@ else
   wget -qO- https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz  | tar xz -C $PHPMYADMIN_DIR --strip-components=1
 fi
 
+echo -e "\e[32mDone!\e[0m"
--- docker/nginx/start.sh
+++ docker/nginx/start.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/ep -v /etc/nginx/conf.d/wordpress.conf
-/usr/local/bin/ep -v /etc/nginx/conf.d/db.wordpress.conf
+/usr/local/bin/ep -v /etc/nginx/includes/*.conf
 
+CERTS_DIR=/etc/nginx/certs
+NGINX_INC_DIR=/etc/nginx/includes
+
+echo "Removing default nginx config..."
+rm -f /etc/nginx/conf.d/default.conf
+
+if [ "$NGINX_ENABLE_SSL" = true ]; then
+  echo "Enabling SSL..."
+
+  if [ ! -f "$CERTS_DIR/server.crt" ]; then
+    echo "Cleaning up old certs..."
+    rm -f "$CERTS_DIR/*"
+
+    echo "Generating new certs..."
+    echo "subjectAltName = DNS:${NGINX_SERVER_NAME}, IP:127.0.0.1" > "$CERTS_DIR/subjectnames.txt"
+    openssl genrsa 2048 > "$CERTS_DIR/server.key"
+    openssl req -new -key "$CERTS_DIR/server.key" -subj "/C=JP/CN=$NGINX_SERVER_NAME" > "$CERTS_DIR/server.csr"
+    openssl x509 -days 3650 -req -extfile "$CERTS_DIR/subjectnames.txt" -signkey "$CERTS_DIR/server.key" < "$CERTS_DIR/server.csr" > "$CERTS_DIR/server.crt"
+  fi
+
+  echo "Generating nginx configuration for ssl..."
+
+  if [ "$NGINX_FORCE_HTTPS" = true ]; then
+    echo "Forcing HTTPS..."
+    cat "$NGINX_INC_DIR/wordpress_force_https.conf" > "$NGINX_INC_DIR/wordpress_http_context.conf"
+  else
+    cat "$NGINX_INC_DIR/wordpress_locations.conf" > "$NGINX_INC_DIR/wordpress_http_context.conf"
+  fi
+else
+  echo "Generating nginx configuration for http..."
+  cat "$NGINX_INC_DIR/wordpress_locations.conf" > "$NGINX_INC_DIR/wordpress_http_context.conf"
+  cat "" > "$NGINX_INC_DIR/wordpress_https_server.conf"
+fi
+
+echo "-----------------------------------------------------------"
+echo "Dont forget to add the following to your '/etc/hosts' file:"
+echo "127.0.0.1 ${NGINX_SERVER_NAME}"
+echo "127.0.0.1 db.${NGINX_SERVER_NAME}"
+echo "-----------------------------------------------------------"
+
+echo "Starting nginx..."
 exec nginx -g 'daemon off;'
